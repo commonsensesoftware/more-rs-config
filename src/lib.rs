99060972ad276cba23c2b5bb0a93628f2c6945a9
Chris Martinez <sydefekt@hotmail.com>
2023-11-23 11:00:22 -0800

    Configure rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 #![doc = include_str!("README.md")]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
 /// Represents the type alias for a configuration value.
 #[cfg(not(feature = "async"))]
 pub type Value = std::rc::Rc<String>;
@@ -60,30 +62,39 @@ pub use section::ConfigurationSection;
 pub use source::*;
 
 #[cfg(feature = "util")]
+#[cfg_attr(docsrs, doc(cfg(feature = "util")))]
 pub use util::*;
 
 #[cfg(feature = "chained")]
+#[cfg_attr(docsrs, doc(cfg(feature = "chained")))]
 pub use chained::{ChainedConfigurationProvider, ChainedConfigurationSource};
 
 #[cfg(feature = "std")]
+#[cfg_attr(docsrs, doc(cfg(feature = "std")))]
 pub use default::*;
 
 #[cfg(feature = "mem")]
+#[cfg_attr(docsrs, doc(cfg(feature = "mem")))]
 pub use memory::{MemoryConfigurationProvider, MemoryConfigurationSource};
 
 #[cfg(feature = "env")]
+#[cfg_attr(docsrs, doc(cfg(feature = "env")))]
 pub use env::{EnvironmentVariablesConfigurationProvider, EnvironmentVariablesConfigurationSource};
 
 #[cfg(feature = "ini")]
+#[cfg_attr(docsrs, doc(cfg(feature = "ini")))]
 pub use ini::{IniConfigurationProvider, IniConfigurationSource};
 
 #[cfg(feature = "json")]
+#[cfg_attr(docsrs, doc(cfg(feature = "json")))]
 pub use json::{JsonConfigurationProvider, JsonConfigurationSource};
 
 #[cfg(feature = "cmd")]
+#[cfg_attr(docsrs, doc(cfg(feature = "cmd")))]
 pub use cmd::{CommandLineConfigurationProvider, CommandLineConfigurationSource};
 
 #[cfg(feature = "xml")]
+#[cfg_attr(docsrs, doc(cfg(feature = "xml")))]
 pub use xml::{XmlConfigurationProvider, XmlConfigurationSource};
 
 /// Contains configuration extension methods.
@@ -92,30 +103,39 @@ pub mod ext {
     use super::*;
 
     #[cfg(feature = "chained")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "chained")))]
     pub use chained::ext::*;
 
     #[cfg(feature = "env")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "env")))]
     pub use env::ext::*;
 
     #[cfg(feature = "ini")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "ini")))]
     pub use ini::ext::*;
 
     #[cfg(feature = "json")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "json")))]
     pub use json::ext::*;
 
     #[cfg(feature = "mem")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "mem")))]
     pub use memory::ext::*;
 
     #[cfg(feature = "cmd")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "cmd")))]
     pub use cmd::ext::*;
 
     #[cfg(feature = "xml")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "xml")))]
     pub use super::xml::ext::*;
 
     #[cfg(feature = "binder")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "binder")))]
     pub use binder::*;
 
     #[cfg(feature = "binder")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "binder")))]
     pub use de::*;
 
     pub use section::ext::*;
